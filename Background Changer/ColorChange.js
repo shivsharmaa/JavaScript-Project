bc1729d32a9a74165a9bb098e7656f78feae531c
Shivam Sharma <sharmaa.shiv54@gmail.com>
2024-07-17 18:28:00 +0530

    background color changer

---
+++ Background Changer/ColorChange.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.btn');
+const body = document.querySelector("body");
+
+buttons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click', function(e){
+        // console.log(e);
+        // console.log(e.target);
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'lime'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+
+})
